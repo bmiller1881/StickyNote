16cc6b31b3756bd7de732d28d64f2baf3d52c89a
Brandon Miller <bmiller1881@gmail.com>
2022-02-18 09:06:53 -0800

    edited package.json homepage

---
+++ build/precache-manifest.51b62581ab25ceb60bb6c2cd648a1854.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d8570ed0b01c80efc3e1c1e277f070",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "1454335c0b69dfd4cc17",
+    "url": "/Keeper-App/static/js/2.29359291.chunk.js"
+  },
+  {
+    "revision": "9d5ca8e03211ede6e245",
+    "url": "/Keeper-App/static/js/main.81665f28.chunk.js"
+  },
+  {
+    "revision": "b556a7f24f06595f9003",
+    "url": "/Keeper-App/static/js/runtime-main.b7e645fa.js"
+  }
+]);
